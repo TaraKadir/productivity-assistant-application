364ffd0ed969d54ff18a20faa9f8f4365e360a09
Lo-river <loriver.loof@gmail.com>
2025-02-17 21:08:09 +0100

    Created add habit button in js

---
+++ js/habits.js
@@ -0,0 +1,6 @@
+let habitContainer = document.createElement("button");
+habitContainer.classList.add("habit-container");
+habitContainer.textContent = "Add your habit"
+
+document.body.appendChild(habitContainer);
+document.querySelector(".some-container").appendChild(habitContainer);
